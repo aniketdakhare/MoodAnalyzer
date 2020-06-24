3a02cc9c12f32f8ed4e15888a45de354d47d7b90
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-25 04:30:36 +0530

    ADDED "ENTERED_WRONG_FIELD" to enum

--- src/main/java/com/bridgelabz/moodanalyzer/exception/MoodAnalysisException.java
+++ src/main/java/com/bridgelabz/moodanalyzer/exception/MoodAnalysisException.java
@@ -5,7 +5,7 @@ public class MoodAnalysisException extends Exception
     public enum ErrorType
     {
         ENTERED_NULL_VALUE, ENTERED_EMPTY_VALUE, ENTERED_WRONG_CLASS_NAME, ENTERED_WRONG_CONSTRUCTOR_PARAMETER,
-        ENTERED_WRONG_METHOD_NAME
+        ENTERED_WRONG_METHOD_NAME,ENTERED_WRONG_FIELD
     }
 
     ErrorType type;
