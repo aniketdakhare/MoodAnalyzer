9c27e9fde13eced888b283b38d540b36a9ab5097
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-25 02:52:48 +0530

    REFACTORED code to get custom exception when wrong method name is passed

--- src/main/java/com/bridgelabz/moodanalyzer/utility/MoodAnalysisReflector.java
+++ src/main/java/com/bridgelabz/moodanalyzer/utility/MoodAnalysisReflector.java
@@ -4,11 +4,14 @@ import com.bridgelabz.moodanalyzer.exception.MoodAnalysisException;
 import com.bridgelabz.moodanalyzer.service.MoodAnalysis;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MoodAnalysisReflector <T>
 {
     static String name = "com.bridgelabz.moodanalyzer.service.MoodAnalysis";
+    static String method= "analyzeMood";
 
     public static <T> MoodAnalysis createMoodAnalyser(String className,T massage)
     {
@@ -40,9 +43,28 @@ public class MoodAnalysisReflector <T>
         return null;
     }
 
-    public static <T> String invokeAnalyzeMood(String className,T massage) throws MoodAnalysisException
+    public static <T> String invokeAnalyzeMood(String className,String methodName, T massage)
     {
-        MoodAnalysis moodAnalysis = createMoodAnalyser(className, massage);
-        return moodAnalysis.analyzeMood();
+        String mood = null;
+        try
+        {
+            Method methods;
+            if (method.equals(methodName))
+                methods = MoodAnalysis.class.getMethod(methodName);
+            else
+                throw new MoodAnalysisException(MoodAnalysisException.ErrorType.ENTERED_WRONG_METHOD_NAME,
+                        "No Such Method Error");
+            MoodAnalysis moodAnalysis = createMoodAnalyser(className, massage);
+            mood = (String) methods.invoke(moodAnalysis);
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
+        {
+            e.printStackTrace();
+        }
+        catch (MoodAnalysisException e)
+        {
+            e.printStackTrace();
+        }
+        return mood;
     }
 }
