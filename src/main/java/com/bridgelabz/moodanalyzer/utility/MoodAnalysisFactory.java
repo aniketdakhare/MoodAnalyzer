3629ebe33af984721564357bd831ef1012c406d7
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-24 23:30:01 +0530

    REFACTORED code by applying custom exception for improper constructor argument

--- src/main/java/com/bridgelabz/moodanalyzer/utility/MoodAnalysisFactory.java
+++ src/main/java/com/bridgelabz/moodanalyzer/utility/MoodAnalysisFactory.java
@@ -3,31 +3,38 @@ package com.bridgelabz.moodanalyzer.utility;
 import com.bridgelabz.moodanalyzer.exception.MoodAnalysisException;
 import com.bridgelabz.moodanalyzer.service.MoodAnalysis;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class MoodAnalysisFactory
 {
     static String name = "com.bridgelabz.moodanalyzer.service.MoodAnalysis";
 
-    public static MoodAnalysis createMoodAnalyser(String className)
+    public static MoodAnalysis createMoodAnalyser(String className,String massage)
     {
         try
         {
             Class<?> moodAnalysisClass;
             if (name.equals(className))
-            {
                 moodAnalysisClass = Class.forName(className);
-            }
             else
-            {
                 throw new MoodAnalysisException(MoodAnalysisException.ErrorType.ENTERED_WRONG_CLASS_NAME,
                         "No Such Class Error");
-            }
-            Object moodAnalysisObject = moodAnalysisClass.newInstance();
+            Constructor<?> moodAnalysisConstructor = moodAnalysisClass.getConstructor(String.class);
+            Object moodAnalysisObject = null;
+            if (massage == null)
+                moodAnalysisObject = moodAnalysisConstructor.newInstance(massage);
+            else
+                throw new MoodAnalysisException(MoodAnalysisException.ErrorType.ENTERED_WRONG_CONSTRUCTOR_PARAMETER,
+                        "No Such Method Error");
             return (MoodAnalysis) moodAnalysisObject;
         }
-        catch (ClassNotFoundException | IllegalAccessException | InstantiationException e)
+        catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException e)
         {
             e.printStackTrace();
-        } catch (MoodAnalysisException e) {
+        }
+        catch (MoodAnalysisException | IllegalAccessException e)
+        {
             e.printStackTrace();
         }
         return null;
