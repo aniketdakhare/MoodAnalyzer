f6121c1b383607f61da967725f3e945e6bcad720
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-24 21:51:49 +0530

    ADDED custom exception for wrong class name

--- src/main/java/com/bridgelabz/moodanalyzer/utility/MoodAnalysisFactory.java
+++ src/main/java/com/bridgelabz/moodanalyzer/utility/MoodAnalysisFactory.java
@@ -1,19 +1,33 @@
 package com.bridgelabz.moodanalyzer.utility;
 
+import com.bridgelabz.moodanalyzer.exception.MoodAnalysisException;
 import com.bridgelabz.moodanalyzer.service.MoodAnalysis;
 
 public class MoodAnalysisFactory
 {
-    public static MoodAnalysis createMoodAnalyser()
+    static String name = "com.bridgelabz.moodanalyzer.service.MoodAnalysis";
+
+    public static MoodAnalysis createMoodAnalyser(String className)
     {
         try
         {
-            Class<?> moodAnalysisClass = Class.forName("com.bridgelabz.moodanalyzer.service.MoodAnalysis");
+            Class<?> moodAnalysisClass;
+            if (name.equals(className))
+            {
+                moodAnalysisClass = Class.forName(className);
+            }
+            else
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ErrorType.ENTERED_WRONG_CLASS_NAME,
+                        "No Such Class Error");
+            }
             Object moodAnalysisObject = moodAnalysisClass.newInstance();
             return (MoodAnalysis) moodAnalysisObject;
         }
         catch (ClassNotFoundException | IllegalAccessException | InstantiationException e)
         {
+            e.printStackTrace();
+        } catch (MoodAnalysisException e) {
             e.printStackTrace();
         }
         return null;
